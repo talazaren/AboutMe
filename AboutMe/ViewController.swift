fa30e6741b4a05035d89caf5749ae7a09d1500cd
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-02-09 14:37:12 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Tatiana Lazarenko on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
