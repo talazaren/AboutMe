4c27ed30a21d86138031b9864308d498813faf57
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-02-10 14:15:01 +0300

    Second screen UI

--- AboutMe/WelcomeViewController.swift
+++ AboutMe/WelcomeViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 final class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var backgroundGradientView: UIView!
     
     var welcomeMessage: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeLabel.text = welcomeMessage
+        
+        setGradient(
+            withColorOne: UIColor(red: 244/255, green: 166/255, blue: 228/255, alpha: 1),
+            andColorTwo: UIColor(red: 100/255, green: 149/255, blue: 237/255, alpha: 1)
+        )
     }
     
+    private func setGradient(withColorOne colorOne: UIColor, andColorTwo colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
 
+        gradientLayer.shouldRasterize = true
+        
+        gradientLayer.zPosition = -1 // без этого свойства градиент перекрывает остальные элементы, не получилось исправить это по-другому
+        backgroundGradientView.layer.addSublayer(gradientLayer)
+    }
 }
